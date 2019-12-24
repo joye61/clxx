ac7a105dc0be9b85f28025edaf3d87a998cd623f
joye <89065495@qq.com>
2019-12-24 09:46:20 +0800

    add export for useIOSInputFixBug

--- packages/effect/src/index.tsx
+++ packages/effect/src/index.tsx
@@ -1,3 +1,4 @@
 export { useEffectOnce } from "./useEffectOnce";
 export { useInterval } from "./useInterval";
 export { useWindowResize } from "./useWindowResize";
+export { useIOSInputFixBug } from "./useIOSInputFixBug";
