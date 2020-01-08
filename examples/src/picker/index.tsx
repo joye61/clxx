94fa8ae8ec7bf3b46b380cd5e8403bf9c8a46a89
joye <89065495@qq.com>
2020-01-08 18:58:17 +0800

    update picker ui

--- examples/src/picker/index.tsx
+++ examples/src/picker/index.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import "./index.scss";
+import React, { useState } from "react";
+import { Picker, showPicker } from "../../../packages/picker/build";
 
-export default function(){
+/**
+ * 可能存在选取Bug，待真机验证 TODO
+ */
+
+export default function() {
+  const [value, setValue] = useState<{
+    v1: any;
+    v2: any;
+  }>({
+    v1: "",
+    v2: ""
+  });
+
   return (
-    <div></div>
+    <div>
+      <div>
+        <p>1、直接通过组件形式触发</p>
+        <Picker
+          className="Picker"
+          list={[
+            "选项一",
+            "选项二",
+            "选项三",
+            "选项四",
+            "选项五",
+            "选项六",
+            "选项七"
+          ]}
+          onConfirm={(index: number, result: any) => {
+            console.log(`选择了第${index}项`, `选择的值为:${result}`);
+            setValue({ ...value, v1: result });
+          }}
+        >
+          {value.v1 || "点击显示选择器"}
+        </Picker>
+      </div>
+      <div>
+        <p>2、直接通过组件形式触发，但不显示当前选择（不显示标题）</p>
+        <Picker
+          className="Picker"
+          showResult={false}
+          list={[
+            "选项一",
+            "选项二",
+            "选项三",
+            "选项四",
+            "选项五",
+            "选项六",
+            "选项七"
+          ]}
+          onConfirm={(index: number, result: any) => {
+            console.log(`选择了第${index}项`, `选择的值为:${result}`);
+            setValue({ ...value, v2: result });
+          }}
+        >
+          {value.v2 || "点击显示选择器"}
+        </Picker>
+      </div>
+      <div>
+        <p>3、直接通过函数调用形式触发</p>
+        <button
+          onClick={() => {
+            showPicker({
+              list: [
+                "选项一",
+                "选项二",
+                "选项三",
+                "选项四",
+                "选项五",
+                "选项六",
+                "选项七"
+              ],
+              onConfirm(index: number, result: any) {
+                console.log(`选择了第${index}项`, `选择的值为:${result}`);
+                alert(result);
+              }
+            });
+          }}
+        >
+          点击显示选择器
+        </button>
+      </div>
+    </div>
   );
 }
--- examples/src/scrollview/index.tsx
+++ examples/src/scrollview/index.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import "./index.scss";
+import React, { useState } from "react";
+import { ScrollView } from "../../../packages/scrollview/build";
 
-export default function(){
+export default function() {
+  const [num, setNum] = useState<number>(10);
+
+  const showList = (fixNum?: number) => {
+    const showNum = fixNum ?? num;
+    const list: React.ReactNode[] = [];
+    for (let i = 0; i < showNum; i++) {
+      list.push(<p key={i}>测试列表元素{i}</p>);
+    }
+    return list;
+  };
+
   return (
-    <div></div>
+    <div>
+      <div>
+        <p>1、常规滚动容器</p>
+        <ScrollView className="ScrollViewList" height="2rem">
+          {showList(15)}
+        </ScrollView>
+      </div>
+      <div>
+        <p>2、触底加载更多，每次触底加载10条数据</p>
+        <ScrollView
+          className="ScrollViewList"
+          height="2rem"
+          onReachEnd={() => {
+            console.log("到达底部", num);
+            setNum(num + 10);
+          }}
+        >
+          {showList()}
+        </ScrollView>
+      </div>
+      <div>
+        <p>3、滚动容器带自动隐藏的滚动条，滚动条自动隐藏</p>
+        <ScrollView className="ScrollViewList" height="2rem" scrollBar>
+          {showList(15)}
+        </ScrollView>
+      </div>
+      <div>
+        <p>4、滚动容器带自动隐藏的滚动条，滚动条始终存在</p>
+        <ScrollView
+          className="ScrollViewList"
+          height="2rem"
+          scrollBar={{ fade: false }}
+        >
+          {showList(15)}
+          
+        </ScrollView>
+      </div>
+    </div>
   );
 }
