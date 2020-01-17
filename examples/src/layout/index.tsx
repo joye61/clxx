fd81dcd7fe66de353c326f7e911007270ca31d49
joye <89065495@qq.com>
2020-01-17 10:02:35 +0800

    update examples

--- examples/src/layout/index.tsx
+++ examples/src/layout/index.tsx
@@ -5,19 +5,25 @@ import { Grid } from "../../../packages/layout/build/Grid";
 export default function() {
   const [list, setList] = useState<React.ReactNode | null>(null);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setList([<p>测试1</p>, <p>测试2</p>, <p>测试3</p>, <p>测试4</p>]);
-  //   }, 2000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setList([
+        <p className="LGCell">测试1</p>,
+        <p className="LGCell">测试2</p>,
+        <p className="LGCell">测试3</p>,
+        <p className="LGCell">测试4</p>
+      ]);
+    }, 2000);
+  }, []);
 
   return (
     <div>
-
       <h1>网格布局组件Grid</h1>
 
       <div>
-        <p className="LayoutGridTitle">2列（默认），空白0（默认），元素高度自适应</p>
+        <p className="LayoutGridTitle">
+          2列（默认），空白0（默认），元素高度自适应
+        </p>
         <Grid>
           <div>这是一个表格单元</div>
           <div>这是一个表格单元</div>
@@ -28,7 +34,9 @@ export default function() {
         </Grid>
       </div>
       <div>
-        <p className="LayoutGridTitle">2列，空白0.05rem，方形单元格，空白环绕</p>
+        <p className="LayoutGridTitle">
+          2列，空白0.05rem，方形单元格，空白环绕
+        </p>
         <Grid gap="0.05rem" square className="LGWithBorder">
           <div className="LGCell">这是一个表格单元</div>
           <div className="LGCell">这是一个表格单元</div>
@@ -39,7 +47,9 @@ export default function() {
         </Grid>
       </div>
       <div>
-        <p className="LayoutGridTitle">2列，空白0.05rem，方形单元格，没有空白环绕</p>
+        <p className="LayoutGridTitle">
+          2列，空白0.05rem，方形单元格，没有空白环绕
+        </p>
         <Grid gap="0.05rem" square gapSurround={false} className="LGWithBorder">
           <div className="LGCell">这是一个表格单元</div>
           <div className="LGCell">这是一个表格单元</div>
@@ -50,7 +60,9 @@ export default function() {
         </Grid>
       </div>
       <div>
-        <p className="LayoutGridTitle">3列，空白0.05rem，自动高度，没有空白环绕</p>
+        <p className="LayoutGridTitle">
+          3列，空白0.05rem，自动高度，没有空白环绕
+        </p>
         <Grid column={3} gap="0.05rem" gapSurround={false}>
           <div className="LGCell">这是一个表格单元</div>
           <div className="LGCell">这是一个表格单元</div>
@@ -58,6 +70,20 @@ export default function() {
           <div className="LGCell">这是一个表格单元</div>
           <p className="LGCell">你好你好你好你好你好你好你好你好</p>
           <p className="LGCell">测试</p>
+        </Grid>
+      </div>
+      <div>
+        <p className="LayoutGridTitle">
+          3列，空白0.05rem，自动高度，没有空白环绕，动态增加
+        </p>
+        <Grid column={3} gap="0.05rem" gapSurround={false}>
+          <div className="LGCell">这是一个表格单元</div>
+          <div className="LGCell">这是一个表格单元</div>
+          <div className="LGCell">这是一个表格单元</div>
+          <div className="LGCell">这是一个表格单元</div>
+          <p className="LGCell">你好你好你好你好你好你好你好你好</p>
+          <p className="LGCell">测试</p>
+          {list}
         </Grid>
       </div>
     </div>
