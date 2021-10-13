23ba077e8144c832e8cf6250e9507a38119d7e76
joye <89065495@qq.com>
2021-10-13 19:03:34 +0800

    remove comment

--- src/Loading/style.ts
+++ src/Loading/style.ts
@@ -33,33 +33,6 @@ export const style = {
   boxHide: css({
     animation: `${LoadingHide} 200ms`,
   }),
-
-  // boxWithExtra: css({
-  //   padding: vw(30),
-  //   "> div:first-of-type": {
-  //     width: vw(48),
-  //     height: vw(48),
-  //   },
-  //   [`@media (min-width: ${ClxxScreenEnv.CriticalWidth}px)`]: {
-  //     padding: vw(30, true),
-  //     "> div:first-of-type": {
-  //       width: vw(48, true),
-  //       height: vw(48, true),
-  //     },
-  //   },
-  // }),
-
-  // // 默认的额外信息样式
-  // defaultExtra: css({
-  //   color: "#f5f5f5",
-  //   fontSize: vw(24),
-  //   marginLeft: vw(20),
-  //   whiteSpace: "nowrap",
-  //   [`@media (min-width: ${ClxxScreenEnv.CriticalWidth}px)`]: {
-  //     fontSize: vw(28, true),
-  //     marginLeft: vw(20, true),
-  //   },
-  // }),
   boxWithExtra: css(adaptive({ padding: 30 }), {
     "> div:first-of-type": adaptive({
       width: 48,
