4a93fdb70ac44383c446e3425e6b21ba76771c78
joye <89065495@qq.com>
2021-10-21 14:46:08 +0800

    merge v2.0.0 to master

---
+++ scripts/clear.js
@@ -0,0 +1,13 @@
+/**
+ * 编译之前先删除旧的编译目标
+ */
+const path = require("path");
+const fs = require("fs");
+
+fs.rmdir(
+  path.resolve(process.cwd(), "./build"),
+  {
+    recursive: true,
+  },
+  () => {}
+);
