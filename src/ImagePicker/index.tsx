67a1a4285a38d29295122d26bb91f1a891174251
joye <89065495@qq.com>
2021-10-21 14:25:41 +0800

    update master

--- src/ImagePicker/index.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * 简单图片选择器，选取结果需要用户自己处理
- */
-export { SimpleImagePicker } from "./SimpleImagePicker";
-/**
- * 带网格UI的图片选择器，选取结果可以删除
- */
-export { ImagePicker } from "./ImagePicker";
--- src/utils/types.d.ts
+++
@@ -1,4 +0,0 @@
-interface SplitedValue {
-  num: number;
-  unit: string;
-}
