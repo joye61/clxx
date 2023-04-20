8479f30c72700a2cdb3c1a40a07cf2cf080e0851
joye <89065495@qq.com>
2023-04-20 13:54:46 +0800

    export adaptive

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,59 @@
-export { Tick } from "./utils/tick";
-export { setContextValue, getContextValue } from "./context";
+export { Tick } from './utils/tick';
+export { setContextValue, getContextValue } from './context';
 
-export { jsonp } from "./utils/jsonp";
-export { uniqKey } from "./utils/uniqKey";
-export { ago } from "./utils/ago";
-export { GET, POST, sendJSON, sugarSend, sendRequest, buildUrlByOption, registerHostAlias } from "./utils/request";
-export { calendarTable } from "./utils/calendarTable";
-export { Countdown } from "./utils/Countdown";
-export { defaultScroll } from "./utils/defaultScroll";
-export { is } from "./utils/is";
-export { waitFor, waitUntil } from "./utils/wait";
-export { normalizeUnit, splitValue } from "./utils/cssUtil";
-export { createApp, history, getHistory } from "./utils/createApp";
+export { jsonp } from './utils/jsonp';
+export { uniqKey } from './utils/uniqKey';
+export { ago } from './utils/ago';
+export {
+  GET,
+  POST,
+  sendJSON,
+  sugarSend,
+  sendRequest,
+  buildUrlByOption,
+  registerHostAlias,
+} from './utils/request';
+export { calendarTable } from './utils/calendarTable';
+export { Countdown } from './utils/Countdown';
+export { defaultScroll } from './utils/defaultScroll';
+export { is } from './utils/is';
+export { waitFor, waitUntil } from './utils/wait';
+export { normalizeUnit, splitValue, adaptive } from './utils/cssUtil';
+export { createApp, history, getHistory } from './utils/createApp';
 
-export { useInterval } from "./Effect/useInterval";
-export { useTick } from "./Effect/useTick";
+export { useInterval } from './Effect/useInterval';
+export { useTick } from './Effect/useTick';
 
-export { Ago } from "./Ago";
-export { Container } from "./Container";
-export { Flex, FlexItem } from "./Flex";
-export { Row, RowAround, RowBetween, RowCenter, RowEnd, RowEvenly, RowStart } from "./Flex/Row";
-export { Col, ColAround, ColBetween, ColCenter, ColEnd, ColEvenly, ColStart } from "./Flex/Col";
-export { Countdowner } from "./Countdowner";
-export { Overlay } from "./Overlay";
-export { showToast, showUniqToast } from "./Toast";
-export { showDialog } from "./Dialog";
-export { Clickable } from "./Clickable";
-export { Indicator } from "./Indicator";
-export { showLoading, showLoadingAtLeast } from "./Loading";
-export { SafeArea } from "./SafeArea";
-export { AutoGrid } from "./AutoGrid";
-export { showAlert } from "./Alert";
-export { ScrollView } from "./ScrollView";
-export { CarouselNotice } from "./CarouselNotice";
+export { Ago } from './Ago';
+export { Container } from './Container';
+export { Flex, FlexItem } from './Flex';
+export {
+  Row,
+  RowAround,
+  RowBetween,
+  RowCenter,
+  RowEnd,
+  RowEvenly,
+  RowStart,
+} from './Flex/Row';
+export {
+  Col,
+  ColAround,
+  ColBetween,
+  ColCenter,
+  ColEnd,
+  ColEvenly,
+  ColStart,
+} from './Flex/Col';
+export { Countdowner } from './Countdowner';
+export { Overlay } from './Overlay';
+export { showToast, showUniqToast } from './Toast';
+export { showDialog } from './Dialog';
+export { Clickable } from './Clickable';
+export { Indicator } from './Indicator';
+export { showLoading, showLoadingAtLeast } from './Loading';
+export { SafeArea } from './SafeArea';
+export { AutoGrid } from './AutoGrid';
+export { showAlert } from './Alert';
+export { ScrollView } from './ScrollView';
+export { CarouselNotice } from './CarouselNotice';
